ea16624b760b40c00fab0f137acf97e449afcd5c
flechablanca19 <maxmanuel2016@gmail.com>
2023-08-24 22:22:33 -0300

    Agregando Docker Rapiditos

---
+++ Dockers_Rapiditos/VSCodium.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo docker run -d \
+  --name=vscodium \
+  --cap-add=IPC_LOCK \
+  --security-opt seccomp=unconfined `#optional` \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -p 3000:3000 \
+  -p 3001:3001 \
+  -v /path/to/config:/config \
+  --shm-size="1gb" \
+  --restart unless-stopped \
+  lscr.io/linuxserver/vscodium:latest
