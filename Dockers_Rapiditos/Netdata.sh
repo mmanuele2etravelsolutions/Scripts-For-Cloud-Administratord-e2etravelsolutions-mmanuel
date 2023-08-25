7af0ce3abf88baed9c22ec2639551fd97527588a
flechablanca19 <maxmanuel2016@gmail.com>
2023-08-24 22:21:00 -0300

    Agregando Docker Rapiditos

---
+++ Dockers_Rapiditos/Netdata.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo docker run -d --name=netdata \
+  --pid=host \
+  --network=host \
+  -v netdataconfig:/etc/netdata \
+  -v netdatalib:/var/lib/netdata \
+  -v netdatacache:/var/cache/netdata \
+  -v /etc/passwd:/host/etc/passwd:ro \
+  -v /etc/group:/host/etc/group:ro \
+  -v /proc:/host/proc:ro \
+  -v /sys:/host/sys:ro \
+  -v /etc/os-release:/host/etc/os-release:ro \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  --restart unless-stopped \
+  --cap-add SYS_PTRACE \
+  --cap-add SYS_ADMIN \
+  --security-opt apparmor=unconfined \
+  netdata/netdata
